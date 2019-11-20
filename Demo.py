a999c4a99e7af23ad45e72a098b9ddff70486c09
StevenGastroSophy <kaspersky518@gmail.com>
2019-11-20 15:44:47 +0800

    新增產生 xml的 demo

--- Demo.py
+++ Demo.py
@@ -1,6 +1,8 @@
 import os
+import xml.etree.cElementTree as ET
 from ParseDir import parseDir
 from GetDictVizNode import getDictVizNode
+from GetDictXml import getDictXml
 
 testDirName = "0"
 #用 getcwd以獲取腳本路徑而非套件路徑
@@ -8,6 +10,11 @@ currentPath = os.getcwd()
 testPath = os.path.join(currentPath, testDirName)
 testParse = parseDir(testPath)
 
-testViz = getDictVizNode(testDirName, testParse)
 #將目錄結構印成樹狀圖
+testViz = getDictVizNode(testDirName, testParse)
 testViz.visualize()
+
+#產生 xml檔案
+testXml = getDictXml(testDirName, testParse)
+testXmlTree = ET.ElementTree(testXml)
+testXmlTree.write("ParseDir.xml")
